c95d0ef934c438955d4c6db9016b4fff827430fc
poonamsinghkushwah <poonamkushwah91744@gmail.com>
2026-06-13 14:24:46 +0530

    Added Array programs

--- src/com/pk/Array/ArrayDemo1.java
+++ src/com/pk/Array/ArrayDemo1.java
@@ -4,13 +4,14 @@ public class ArrayDemo1 {
 	
 	public static void main(String[] args) {
 		
-		int[] a= {10,20,30,40,50,};
+		int[] a= {10,20,30,40,50,60};
 		
 		System.out.println(a[0]);
 		System.out.println(a[1]);
 		System.out.println(a[2]);
 		System.out.println(a[3]);
 		System.out.println(a[4]);
+		System.out.println(a[5]);
 		
 		System.out.println("Length of array:"+a.length);
 	}
