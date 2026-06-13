33d1854a5aa84b1c0afaecb0d1d4d69b6bae2ac8
poonamsinghkushwah <poonamkushwah91744@gmail.com>
2026-06-13 13:40:55 +0530

    add new files

---
+++ src/com/pk/Encapsulation/EmployeeClient.java
@@ -0,0 +1,15 @@
+package com.pk.Encapsulation;
+
+public class EmployeeClient {
+	
+	public static void main (String[] args) {
+		
+		Employee e = new Employee();
+		
+		 e.setId(101);
+		 e.setName("Poonam Kushwah");
+		 e.setSalary(50000);
+		
+	}
+
+}
